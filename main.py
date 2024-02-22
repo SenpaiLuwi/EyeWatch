e4de30862fb237f4d4257ded7d972b71d418f7cf
John Louie Ubias <110644790+SenpaiLuwi@users.noreply.github.com>
2024-02-22 11:27:53 +0800

    Removed Login and Sign up Function

--- main.py
+++ main.py
@@ -24,56 +24,18 @@ class FaceCaptureApp:
 
         self.user_info = {}
 
-        self.login_frame = ttk.Frame(self.root)
-        self.signup_frame = ttk.Frame(self.root)
         self.home_frame = ttk.Frame(self.root)
 
         self.current_frame = None
 
         self.image_location_entry = ttk.Entry(self.home_frame, state="readonly", textvariable=tk.StringVar(value=""))
 
-        self.create_login_widgets()
-        self.create_signup_widgets()
         self.create_home_widgets()
 
-        self.show_frame(self.login_frame)
+        self.show_frame(self.home_frame)
 
         self.create_configuration_file()
         self.load_configuration()
-
-    def create_login_widgets(self):
-        self.email_label_login = ttk.Label(self.login_frame, text="Email:")
-        self.email_entry_login = ttk.Entry(self.login_frame)
-        self.password_label_login = ttk.Label(self.login_frame, text="Password:")
-        self.password_entry_login = ttk.Entry(self.login_frame, show="*")
-        self.login_button = ttk.Button(self.login_frame, text="Login", command=self.login)
-        self.signup_page_button = ttk.Button(self.login_frame, text="Sign Up", command=self.show_signup_page)
-
-        self.email_label_login.grid(row=0, column=0, pady=(10, 0), padx=(10, 0), sticky="e")
-        self.email_entry_login.grid(row=0, column=1, pady=(10, 0), padx=(0, 10), sticky="w")
-        self.password_label_login.grid(row=1, column=0, pady=10, padx=(10, 0), sticky="e")
-        self.password_entry_login.grid(row=1, column=1, pady=10, padx=(0, 10), sticky="w")
-        self.login_button.grid(row=2, column=0, columnspan=2, pady=10, padx=10, sticky="nsew")
-        self.signup_page_button.grid(row=3, column=0, columnspan=2, pady=(0, 10), padx=10, sticky="nsew")
-
-        self.login_frame.grid_rowconfigure(4, weight=1)
-        self.login_frame.grid_columnconfigure(2, weight=1)
-
-        ttk.Label(self.login_frame, text="").grid(row=4, column=1)
-        ttk.Label(self.login_frame, text="").grid(row=4, column=2)
-
-    def create_signup_widgets(self):
-        self.email_label_signup = ttk.Label(self.signup_frame, text="Email:")
-        self.email_entry_signup = ttk.Entry(self.signup_frame)
-        self.password_label_signup = ttk.Label(self.signup_frame, text="Password:")
-        self.password_entry_signup = ttk.Entry(self.signup_frame, show="*")
-        self.signup_button = ttk.Button(self.signup_frame, text="Sign Up", command=self.signup)
-
-        self.email_label_signup.grid(row=0, column=0, pady=10, padx=10)
-        self.email_entry_signup.grid(row=0, column=1, pady=10, padx=10)
-        self.password_label_signup.grid(row=1, column=0, pady=10, padx=10)
-        self.password_entry_signup.grid(row=1, column=1, pady=10, padx=10)
-        self.signup_button.grid(row=2, column=0, columnspan=2, pady=10, padx=10)
 
     def create_home_widgets(self):
         self.camera_dropdown = ttk.Combobox(self.home_frame, textvariable=self.selected_camera, values=self.camera_names)
@@ -89,21 +51,18 @@ class FaceCaptureApp:
         self.home_button.grid(row=0, column=1, pady=10, padx=10)
 
         self.tools_button = ttk.Button(self.home_frame, text="Tools", command=self.stop_capture_function)
-        self.tools_button.grid(row=0, column=2, pady=10, padx=10)
-
-        self.user_button = ttk.Button(self.home_frame, text="User Info", command=self.stop_capture_function)
-        self.user_button.grid(row=0, column=3, pady=10, padx=10)
+        self.tools_button.grid(row=0, column=3, pady=10, padx=10)
 
         self.save_image_location_button = ttk.Button(self.home_frame, text="Save Image Location", command=self.set_image_location)
         self.save_image_location_button.grid(row=4, column=0, pady=10, padx=10)
 
         self.save_video_location_button = ttk.Button(self.home_frame, text="Save Video Location", command=self.set_video_location)
-        self.save_video_location_button.grid(row=5, column=0, pady=10, padx=10)
+        self.save_video_location_button.grid(row=6, column=0, pady=10, padx=10)
 
         self.image_location_label = ttk.Label(self.home_frame, text="Image Save Location:")
         self.image_location_entry = ttk.Entry(self.home_frame, state="readonly", textvariable=tk.StringVar(value=""))
-        self.image_location_label.grid(row=6, column=0, pady=10, padx=10, sticky="e")
-        self.image_location_entry.grid(row=6, column=1, pady=10, padx=10, sticky="w")
+        self.image_location_label.grid(row=5, column=0, pady=10, padx=10, sticky="e")
+        self.image_location_entry.grid(row=5, column=1, pady=10, padx=10, sticky="w")
 
         self.video_location_label = ttk.Label(self.home_frame, text="Video Save Location:")
         self.video_location_entry = ttk.Entry(self.home_frame, state="readonly", textvariable=tk.StringVar(value=""))
@@ -135,6 +94,7 @@ class FaceCaptureApp:
 
         except FileNotFoundError:
             self.create_configuration_file()
+
     def save_configuration(self):
         config_data = {"image_location": self.image_location_entry.get()}
 
@@ -161,36 +121,6 @@ class FaceCaptureApp:
             self.current_frame.grid_forget()
         frame.grid(row=0, column=0, sticky="nsew")
         self.current_frame = frame
-
-    def show_signup_page(self):
-        self.show_frame(self.signup_frame)
-
-    def signup(self):
-        username = self.email_entry_signup.get()
-        password = self.password_entry_signup.get()
-
-        if username and password:
-            if username not in self.user_info:
-                self.user_info[username] = password
-                messagebox.showinfo("Signup Successful", "Account created successfully!")
-                self.show_frame(self.login_frame)
-            else:
-                messagebox.showerror("Error", "Username already exists. Please choose another username.")
-        else:
-            messagebox.showerror("Error", "Please enter both email and password for signup.")
-
-    def login(self):
-        username = self.email_entry_login.get()
-        password = self.password_entry_login.get()
-
-        if username and password:
-            if username in self.user_info and self.user_info[username] == password:
-                messagebox.showinfo("Login Successful", "Welcome to the EyeWatch!")
-                self.show_frame(self.home_frame)
-            else:
-                messagebox.showerror("Login Failed", "Incorrect username or password.")
-        else:
-            messagebox.showerror("Error", "Please enter both email and password for login.")
 
     def detect_cameras(self):
         camera_list = []
